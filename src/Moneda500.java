c1f374f625e8eb1dc089dfa7f46264e16080d86b
Gerhec <gerhecp@gmail.com>
2026-05-02 20:23:00 -0400

    comentarios (javadoc)

--- src/Moneda500.java
+++ src/Moneda500.java
@@ -1,8 +1,18 @@
+/**
+ * Representa una moneda física de 500 pesos que el comprador usará para pagar en la maquina.
+ */
 public class Moneda500 extends Moneda {
+    /**
+     * Construye una nueva moneda de 500 pesos.
+     */
     public Moneda500() {
         super();
     }
-
+    /**
+     * Nos entrega el valor de esta moneda.
+     *
+     * @return Siempre devuelve 500.
+     */
     @Override
     public int getValor() {
         return 500;
