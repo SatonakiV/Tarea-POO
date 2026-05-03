8c34b3b6b3525bbe190c971bbcb385b576987188
Gerhec <gerhecp@gmail.com>
2026-05-02 20:13:45 -0400

    comentarios (javadoc)

--- src/Moneda.java
+++ src/Moneda.java
@@ -1,14 +1,40 @@
+/**
+ * Representa una moneda genérica que el comprador usa para intentar pagar en la máquina,
+ * sirve para agrupar a cualquier moneda de cualquier valor, nos permite
+ * compararlas u ordenarlas de menor a mayor valor gracias a comparable.
+ */
+
 public abstract class Moneda implements Comparable<Moneda> {
+    /**
+     * Construye una nueva moneda como base.
+     */
 
     public Moneda() {
     }
-
+    /**
+     * Obtiene el valor numérico de esta moneda.
+     *
+     * @return El valor de la moneda en formato entero.
+     */
     public abstract int getValor();
-
+    /**
+     * Obtiene un identificador único para la moneda
+     * como en la vida real cada moneda es distinta, usamos su espacio en la memoria (ToString)
+     * para simular que hay un número de serie.
+     *
+     * @return Un String que representa la serie de la moneda.
+     */
     public String getSerie() {
         return this.toString();
     }
 
+    /**
+     * Compara el valor de esta moneda con el de otra que le pasemos.
+     * Así la máquina o el comprador puede saber fácilmente cuál vale más.
+     * 
+     * @param m la moneda con la que queremos comparar.
+     * @return Un numero negativo si esta vale menos, 0 si valen lo mismo, o positivo si vale más.
+     */
     @Override
     public int compareTo(Moneda m) {
         return Integer.compare(this.getValor(), m.getValor());
