d52ccd741b739db1de7be8981e2438fb96ca64c2
Gerhec <gerhecp@gmail.com>
2026-05-02 20:49:14 -0400

    comentarios (javadoc)

--- src/Precios.java
+++ src/Precios.java
@@ -1,3 +1,8 @@
+/**
+ * Guarda los precios fijos de las cosas que vende la máquina.
+ * Usamos un enum para no tener que andar escribiendo los números a mano en el código
+ * y tener todos los valores ordenados en un solo lugar.
+ */
 public enum Precios {
 
     CocaCola(1000),
@@ -12,10 +17,18 @@ public enum Precios {
     Chocman(600);
 
     private final int precio;
-
+    /**
+     * Le da el valor en pesos a cada producto de la lista.
+     *
+     * @param precio El costo del producto.
+     */
     Precios(int precio) {
         this.precio = precio;
-
+/**
+ * Sirve para ver cuánto cuesta el producto que elegimos.
+ *
+ * @return El precio del producto en números enteros.
+ */
     }
     public int getPrecio() {
         return precio;
