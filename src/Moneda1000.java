f40dd96158fe52b3c4d05467c754fe3357116434
Gerhec <gerhecp@gmail.com>
2026-05-02 20:25:37 -0400

    comentarios (javadoc)

--- src/Moneda1000.java
+++ src/Moneda1000.java
@@ -1,8 +1,18 @@
+/**
+ * Representa una moneda física de 1000 pesos que el comprador usará para pagar en la maquina.
+ */
 public class Moneda1000 extends Moneda {
+    /**
+     * Construye una nueva moneda de 1000 pesos.
+     */
     public Moneda1000() {
         super();
     }
-
+    /**
+     * Nos entrega el valor de esta moneda.
+     *
+     * @return siempre devuelve 1000.
+     */
     @Override
     public int getValor() {
         return 1000;
